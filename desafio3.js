5541fc5ab9f71a323f738fabcc2eefa6670be8d2
guilherme victor bezerra frota <victorbezerrafrota@gmail.com>
2025-03-14 03:23:59 -0300

    desafio3 concluido

--- desafio3.js
+++ desafio3.js
@@ -1,20 +1,16 @@
 //Crie um programa que gere 6 números aleatórios entre 1 e 60 para um sorteio da Mega-Sena.
 
-let numerosAleatorios = [];
+function gerar_numero_mega_cena() {
+    const quantidade_de_numeros = 6
+    let numeros_sorteados = new Set(); // nao permite valores repitidos(automaticamente impede que valores repitidos sejam inseridos)
 
-
-function geradorDeNumero (){
-    return parseInt(Math.random() * 60 + 1);
-}
-
-function imputNumero(){
-    let quantidade = 6;
-    let numeroRandom =0;
-    for (let repetidor = 0; repetidor < quantidade; repetidor++) {
-        if (numeroRandom = geradorDeNumero()){
-        numerosAleatorios.push(numeroRandom);
-        }
+    while (numeros_sorteados.size < quantidade_de_numeros) { 
+        let numero = Math.floor(Math.random() * 60) + 1;
+        numeros_sorteados.add(numero); // Se o número já existir, ele não será adicionado novamente
     }
+
+    return [...numeros_sorteados].sort((a, b) => a - b); // Converte o Set em array e ordena
 }
 
-console.log(numerosAleatorios);
+console.log(gerar_numero_mega_cena());
+
